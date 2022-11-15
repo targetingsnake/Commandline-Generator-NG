068d6a016c616d54953f45b887bdddb944bc5011
targetingsnake <targetingsnake@users.noreply.github.com>
2022-11-15 12:07:29 +0100

    implemented JSON parser and basic threading

--- Backend/Commandline-Generator-Backend/Commandline-Generator-Backend/Program.cs
+++ Backend/Commandline-Generator-Backend/Commandline-Generator-Backend/Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Commandline_Generator_Backend
+{
+    class HttpServer
+    {
+        public static HttpListener listener;
+        public static string url = "http://localhost:8000/";
+
+        public static async Task HandleIncomingConnections()
+        {
+            bool runServer = true;
+
+            while (runServer)
+            {
+                // Will wait here until we hear from a connection
+                HttpListenerContext ctx = await listener.GetContextAsync();
+
+                Thread Worker = new Thread(RequestHandler.HandleRequest);
+                Worker.Start(ctx);
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            // Create a Http server and start listening for incoming connections
+            listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Listening for connections on {0}", url);
+
+            // Handle requests
+            Task listenTask = HandleIncomingConnections();
+            listenTask.GetAwaiter().GetResult();
+
+            // Close the listener
+            listener.Close();
+        }
+    }
+}
---
+++ Backend/Commandline-Generator-Backend/Commandline-Generator-Backend/RequestHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Commandline_Generator_Backend
+{
+    public class Test
+    {
+        public int test { get; set; }
+    }
+
+    public class Response
+    {
+        public Response(int intCode, object obj)
+        {
+            code = intCode;
+            result = intCode == 0 ? "ack" : "nack";
+            data = obj;
+        }
+        public string result { get; set; }
+        public int code { get; set; }
+        public object data { get; set; }
+    }
+
+    internal class RequestHandler
+    {
+        public static void HandleRequest(object obj)
+        {
+            HttpListenerContext ctx = obj as HttpListenerContext;
+            HttpListenerRequest req = ctx.Request;
+            HttpListenerResponse resp = ctx.Response;
+
+            if (!((req.HttpMethod == "GET" || req.HttpMethod == "POST") && req.HasEntityBody))
+            {
+                sendResponse(1, null, resp);
+                return;
+            }
+
+            Stream body = req.InputStream;
+            Encoding encoding = req.ContentEncoding;
+            StreamReader reader = new System.IO.StreamReader(body, encoding);
+            string s = reader.ReadToEnd();
+            Test? test = JsonSerializer.Deserialize<Test>(s);
+
+            Console.WriteLine(req.HttpMethod + " " + req.Url.ToString() + " " + req.UserHostName + " " + req.UserAgent);
+
+            sendResponse(0, null, resp);
+        }
+
+        private static async void sendResponse(int code, object obj, HttpListenerResponse resp)
+        {
+            Response respData = new Response(code, obj);
+            string result = JsonSerializer.Serialize(respData);
+            byte[] data = Encoding.UTF8.GetBytes(result);
+            resp.ContentType = "text/json";
+            resp.ContentEncoding = Encoding.UTF8;
+            resp.ContentLength64 = data.LongLength;
+            await resp.OutputStream.WriteAsync(data, 0, data.Length);
+            resp.Close();
+        }
+    }
+}
